f0ff88dcdfcbc9b6c239c6191409ea7d50e623b4
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-11-10 12:06:48 +0300

    pre-rendering and fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi!, Iam Isaac, a Software Engineer and Problem Solver. You can contact me on <Link href="">Twitter</Link></p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
