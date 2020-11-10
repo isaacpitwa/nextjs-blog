3904eb2423b8734e18abfed63506bdbecdd60eac
Isaac Pitwa <isaacpitwa256@gmail.com>
2020-11-10 12:54:49 +0300

    Polish post page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi!, Iam Isaac, a Software Engineer and Problem Solver. You can contact me on <Link href="">Twitter</Link></p>
+        <p>Hi!, Iam Isaac, a Software Engineer and Problem Solver. You can contact me on <Link href="https://twitter.com/isaacpitwa">Twitter</Link></p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
